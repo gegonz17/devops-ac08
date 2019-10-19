3c31e755bde83f12a280ff8472dc3be8bc5d0981
Gerson Callisaya Gonzales <1900832@SALAS.AULAS>
2019-10-18 21:29:06 -0300

    Foi

--- com/faculdadeimpacta/calculadora/test/test_app_web_start.py
+++ com/faculdadeimpacta/calculadora/test/test_app_web_start.py
@@ -2,16 +2,16 @@ from com.faculdadeimpacta.calculadora import app_web_start
 
 def test_root_status():
 
-instancia_app = app_web_start.app.test_client()
+app_web_start = app_web_start.app.test_client()
 
-response = instancia_app.get('/')
+response = app_web_start.get('/')
 
 assert response.status_code == 200, 'Deveria existir essa rota'
 
 def test_root_url():
 
-instancia_app = app_web_start.app.test_client()
+app_web_start = app_web_start.app.test_client()
 
-response = instancia_app.get('/')
+response = app_web_start.get('/')
 
 assert response.data.decode('utf-8') == 'Index Page', 'Deveria ser Index Page'
